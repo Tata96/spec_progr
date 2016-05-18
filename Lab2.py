6c76df8f4f15b6b66b06d71b2d023a372267c8e7
Tata96 <NY-96@ya.ru>
2016-05-18 20:52:05 +0300

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -5,54 +5,54 @@ import pandas
 class VHIApp(server.App):
     title = "VHI App"
 
-    inputs = [{"type": 'dropdown', "label": 'Часовий ряд', "value": 'VHI',
+    inputs = [{"type": 'dropdown', "label": 'Time row', "value": 'VHI',
                "options": [{"label": "VHI", "value": "VHI"},
                            {"label": "VCI", "value": "VCI"},
                            {"label": "TCI", "value": "TCI"}], "key": 'number'},
 
-              {"type": 'dropdown', "label": 'Область', "value": 'Київська',
-               "options": [{"label": "Вінницька", "value": "Вінницька"},
-                           {"label": "Волинська", "value": "Волинська"},
-                           {"label": "Дніпропетровська", "value": "Дніпропетровська"},
-                           {"label": "Донецька", "value": "Донецька"},
-                           {"label": "Житомирська", "value": "Житомирська"},
-                           {"label": "Закарпатська", "value": "Закарпатська"},
-                           {"label": "Запорізька", "value": "Запорізька"},
-                           {"label": "Івано-Франківська", "value": "Івано-Франківська"},
-                           {"label": "Київська", "value": "Київська"},
-                           {"label": "Кіровоградська", "value": "Кіровоградська"},
-                           {"label": "Львівська", "value": "Львівська"},
-                           {"label": "Миколаївська", "value": "Миколаївська"},
-                           {"label": "Одеська", "value": "Одеська"},
-                           {"label": "Полтавська", "value": "Полтавська"},
-                           {"label": "Рівненська", "value": "Рівненська"},
-                           {"label": "Сумська", "value": "Сумська"},
-                           {"label": "Тернопільська", "value": "Тернопільська"},
-                           {"label": "Харківська", "value": "Харківська"},
-                           {"label": "Херсонська", "value": "Херсонська"},
-                           {"label": "Хмельницька", "value": "Хмельницька"},
-                           {"label": "Черкаська", "value": "Черкаська"},
-                           {"label": "Чернівецька", "value": "Чернівецька"},
-                           {"label": "Чернігівська", "value": "Чернігівська"},
-                           {"label": "Республіка Крим", "value": "Республіка Крим"}], "key": 'region'},
+              {"type": 'dropdown', "label": 'District', "value": 'Kiev',
+               "options": [{"label": "Vinnytsya", "value": "Vinnytsya"},
+                           {"label": "Volyn", "value": "Volyn"},
+                           {"label": "Dnipropetrovsk", "value": "Dnipropetrovsk"},
+                           {"label": "Donetsk", "value": "Donetsk"},
+                           {"label": "Zhytomyr", "value": "Zhytomyr"},
+                           {"label": "Zacarpathia", "value": "Zacarpathia"},
+                           {"label": "Zaporizhzhya", "value": "Zaporizhzhya"},
+                           {"label": "Ivano-Frankivsk", "value": "Ivano-Frankivsk"},
+                           {"label": "Kiev", "value": "Kiev"},
+                           {"label": "Kirovograd", "value": "Kirovograd"},
+                           {"label": "Lviv", "value": "Lviv"},
+                           {"label": "Mykolayiv", "value": "Mykolayiv"},
+                           {"label": "Odessa", "value": "Odessa"},
+                           {"label": "Poltava", "value": "Poltava"},
+                           {"label": "Rivne", "value": "Rivne"},
+                           {"label": "Sumy", "value": "Sumy"},
+                           {"label": "Ternopil", "value": "Ternopil"},
+                           {"label": "Kharkiv", "value": "Kharkiv"},
+                           {"label": "Kherson", "value": "Kherson"},
+                           {"label": "Khmel'nyts'kyy", "value": "Khmel'nyts'kyy"},
+                           {"label": "Cherkasy", "value": "Cherkasy"},
+                           {"label": "Chernivtsi", "value": "Chernivtsi"},
+                           {"label": "Chernihiv", "value": "Chernihiv"},
+                           {"label": "Crimea", "value": "Crimea"}], "key": 'region'},
 
-              {"type": "slider", "label": 'Товщина лінії', "key": "width", "value": '1',
+              {"type": "slider", "label": 'Width of line', "key": "width", "value": '1',
                "min": 1, "max": 10, "action_id": "width"},
-              {"type": "text", "label": 'Рік #1', "key": "year1", "value": '2007'},
-              {"type": "text", "label": 'Рік #2', "key": "year2", "value": '2008'},
-              {"type": "text", "label": 'Інтервал тижнів, від', "key": "w1", "value": '1'},
-              {"type": "text", "label": 'до', "key": "w2", "value": '52'}]
+              {"type": "text", "label": 'Year #1', "key": "year1", "value": '2007'},
+              {"type": "text", "label": 'Year #2', "key": "year2", "value": '2008'},
+              {"type": "text", "label": 'Interval of weeks from', "key": "w1", "value": '1'},
+              {"type": "text", "label": 'to', "key": "w2", "value": '52'}]
 
-    controls = [{"type": "button", "label": "Побудувати графіки", "id": "submit_plot"}]
-    tabs = ["Графік1", "Графік2", "Таблиця1", "Таблиця2"]
-    outputs = [{"type": "plot", "id": "plot", "control_id": "submit_plot", "tab": "Графік1"},
-               {"type": "plot", "id": "plot1", "control_id": "submit_plot", "tab": "Графік2"},
-               {"type": "table", "id": "table", "control_id": "submit_plot", "tab": "Таблиця1", "on_page_load": True},
-               {"type": "table", "id": "table1", "control_id": "submit_plot", "tab": "Таблиця2", "on_page_load": True}]
+    controls = [{"type": "button", "label": "Create a graph", "id": "submit_plot"}]
+    tabs = ["Graph1", "Graph2", "Table1", "Table2"]
+    outputs = [{"type": "plot", "id": "plot", "control_id": "submit_plot", "tab": "Graph1"},
+               {"type": "plot", "id": "plot1", "control_id": "submit_plot", "tab": "Graph2"},
+               {"type": "table", "id": "table", "control_id": "submit_plot", "tab": "Table1", "on_page_load": True},
+               {"type": "table", "id": "table1", "control_id": "submit_plot", "tab": "Table2", "on_page_load": True}]
 
     def table(self, params):
         check_params(params)
-        df = pandas.read_csv("E:\Programs\Учьобка ((\СРП\Lab2\data.csv", encoding='utf-8')
+        df = pandas.read_csv("/home/KPI2course/SRP/Lab2/data.csv", encoding='utf-8')
         df = df.drop('Unnamed: 0', 1)
         df = df[(df['region'] == params['region']) & (df['year'] == int(params['year1'])) &
                 (df['week'] >= int(params['w1'])) & (df['week'] <= int(params['w2']))]
@@ -60,7 +60,7 @@ class VHIApp(server.App):
 
     def table1(self, params):
         check_params(params)
-        df = pandas.read_csv("E:\Programs\Учьобка ((\СРП\Lab2\data.csv", encoding='utf-8')
+        df = pandas.read_csv("/home/KPI2course/SRP/Lab2/data.csv", encoding='utf-8')
         df = df.drop('Unnamed: 0', 1)
         df = df[(df['region'] == params['region']) & (df['year'] == int(params['year2'])) &
                 (df['week'] >= int(params['w1'])) & (df['week'] <= int(params['w2']))]
@@ -93,3 +93,9 @@ def check_params(params):
 
 app = VHIApp()
 app.launch()
+                          
+
+    
+       
+
+   
